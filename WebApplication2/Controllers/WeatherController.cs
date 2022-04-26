b431a87b650f599eeb0c365f1262815bf0557bfd
DrDeckto <fynjy138@gmail.com>
2022-04-27 00:13:59 +0300

    Add project files.

---
+++ WebApplication2/Controllers/WeatherController.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class WeatherController : Controller
+    {
+        public static Weather weather = new();
+
+        [HttpPost("add value")]
+        public IActionResult AddTemperature(DateTime time, float temperature)
+        {
+            weather.Add(new Weather()
+            {
+                Temperature = temperature,
+                Time = time,
+            });
+
+            return Ok(weather);
+        }
+
+        [HttpGet("get result")]
+        public IActionResult GetResult()
+        {
+            return Ok(weather.Get());
+        }
+
+        [HttpGet("get result after a certain date")]
+        public IActionResult GetResult(DateTime time)
+        {
+            return Ok(weather.Get(time));
+        }
+
+        [HttpPut("update result")]
+        public IActionResult Update(DateTime time, float temperature)
+        {
+            weather.Update(time, temperature);
+            return Ok();
+        }
+
+        [HttpDelete("delete value")]
+        public IActionResult Delete(DateTime time)
+        {
+            weather.Delete(time);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication2/Model/Weather.cs
@@ -0,0 +1,87 @@
+﻿namespace WebApplication2.Model
+{
+    public class Weather
+    {
+        private readonly List<Weather> weatherValueList;
+
+        public Weather()
+        {
+            weatherValueList = new List<Weather>();
+        }
+
+        /// <summary>
+        /// Температура
+        /// </summary>
+        public float Temperature { get; set; }
+
+        /// <summary>
+        /// Время
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Добавить дату и температуру
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add(Weather value)
+        {
+            weatherValueList.Add(value);
+        }
+
+        /// <summary>
+        /// возвращает все данные 
+        /// </summary>
+        /// <returns>string</returns>
+        public string Get()
+        {
+            return weatherValueList.Aggregate("", (current, i) 
+                => current + (i.Time + " " + i.Temperature + "\n"));
+        }
+
+        /// <summary>
+        /// Возвращает все значения после заданного времени
+        /// </summary>
+        /// <param name="time">время</param>
+        /// <returns>string</returns>
+        public string Get(DateTime time)
+        {
+            return weatherValueList.Where(i => i.Time >= time).Aggregate("", (current, i)
+                => current + (i.Time + " " + i.Temperature + "\n"));
+        }
+
+        /// <summary>
+        /// Обновляет значение температуры в указанную дату
+        /// </summary>
+        /// <param name="time">время</param>
+        /// <param name="temperature">температура</param>
+        public void Update(DateTime time, float temperature)
+        {
+            foreach (var i in weatherValueList)
+            {
+                if (i.Time == time)
+                {
+                    Console.WriteLine("OK");
+                    i.Temperature = temperature;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// удаляет значение по времени
+        /// </summary>
+        /// <param name="time">время</param>
+        public void Delete(DateTime time)
+        {
+            foreach (var i in weatherValueList)
+            {
+                if (i.Time == time)
+                {
+                    Console.WriteLine("Delete");
+                    weatherValueList.Remove(i);
+                    break;
+                }
+            }
+        }
+    }
+}
